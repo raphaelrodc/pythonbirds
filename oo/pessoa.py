b70c81dc919f787d29e706e442c83c576290bba2
Raphael Castro <raphaelrodc@gmail>
2019-12-23 14:35:48 -0300

    Criado atributo complexo filho.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade = 27):
+    def __init__(self, *filhos, nome=None, idade = 27):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
+
 if __name__ == '__main__':
-    p = Pessoa('Arkantus')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Raphael'
-    print(p.nome)
-    print(p.idade)
+    raphael = Pessoa(nome='Raphael')
+    arkantus = Pessoa(raphael,nome='Arkantus')
+    print(Pessoa.cumprimentar(arkantus))
+    print(id(arkantus))
+    print(arkantus.cumprimentar())
+    print(arkantus.nome)
+    print(arkantus.idade)
+    for filho in arkantus.filhos:
+        print(filho.nome)
